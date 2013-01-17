e02d29f0eb3219d37ea402709d0ba22bf26949a3
Faisal N. Jawdat <faisal@faisal.com>
2013-01-16 19:04:57 -0800

    Initial Rails app commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailIntel::Application.config.secret_token = 'd62fb097b9c6d607260e3e3802beb2b4e9c43f6952a16287d9084c39fb1e0c1755fcd26a056b008ba6611d7d60a3b581560bd5d87ef1b272ba4b029d37647fd7'
